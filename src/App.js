d12bcef52ed435d7771fba79aa7aeaa437f0baf6
ikram dev <lalatputih22@gmail.com>
2024-03-14 23:26:39 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Form from './Form';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App my-4 h-screen">
+      <h4 className='text-4xl font-bold'>Approva<span className='text-indigo-600'>Flow</span></h4>  
+      <div className='border-2 w-1/2 h-full rounded-2xl m-auto my-4'> 
+        <Form />
+      </div>
     </div>
   );
 }
---
+++ src/Form.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Form = () => {
+    return (
+        <>
+            <div className="w-full h-full">
+                <input type="text" className="border" placeholder="email"> </input>
+            </div>
+        </>
+    )
+}
+
+export default Form;
